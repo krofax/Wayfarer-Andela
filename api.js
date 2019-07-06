0bc590ed792943c9eeeac38667fa25952bc1bee0
beveloper <bkrofegha@mail.com>
2019-07-06 12:56:34 -0700

    ran the first api endpoint

---
+++ api.js
@@ -0,0 +1,13 @@
+// api.js
+import express from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.status(200).send({ 'message': 'YAY! Congratulations! Start building wayfarer ' });
+})
+
+app.listen(3000)
+console.log('app running on port ', 3000);
