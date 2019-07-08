008171bef2ce682a1dd5cae3bbcea362514f2357
beveloper <bkrofegha@mail.com>
2019-07-07 21:27:45 -0700

    ft(set up) add user model[#167122896]

---
+++ api/models/users.js
@@ -0,0 +1,71 @@
+const users = [
+    {
+        id: 1,
+        user_id: 1,
+        email: 'bkrofax@yahoo.com',
+        first_name: 'pero',
+        last_name: 'daniel',
+        password: 'jumanji',
+        is_admin: false,
+    },
+    {
+        id: 4,
+        email: 'junol@yahoo.com',
+        first_name: 'taiye',
+        last_name: 'gopor',
+        password: 'bambam',
+        is_admin: false,
+    },
+    {
+        id: 5,
+        email: '',
+        first_name: '',
+        last_name: 'emma',
+        password: 'gopor',
+        is_admin: false,
+    },
+    {
+        id: 4,
+        email: 'boths',
+        first_name: 'chuks',
+        last_name: 'emma',
+        password: 'developer',
+        is_admin: false,
+    },
+    {
+        id: 4,
+        email: 'yupor@yahoo.com',
+        first_name: 'chuks',
+        last_name: 'emma',
+        password: 'dev',
+        is_admin: false,
+    },
+    {
+        first_name: 'amaka',
+        last_name: 'chuks',
+        email: 'yupor@yahoo.com',
+        password: 'hjhg'
+    },
+    {
+        email: 'yupor@yahoo.com',
+        password: 'develops'
+    },
+    {
+        email: '',
+        password: 'awesome'
+    },
+    {
+        email: 'boths104',
+        password: 'yuopii'
+    },
+    {
+        email: 'yupor@yahoo.com',
+        password: ''
+    },
+    {
+        email: 'yupor@yahoo.com',
+        password: 'dev'
+    }
+];
+
+export default users;
